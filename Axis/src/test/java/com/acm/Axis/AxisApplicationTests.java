dd862c3127ce19b66479afc73afc3a7798141961
kphilippp <kevinphilip2004@gmail.com>
2025-02-18 00:07:48 -0600

    env_setup

--- Axis/src/test/java/com/acm/Axis/AxisApplicationTests.java
+++ Axis/src/test/java/com/acm/Axis/AxisApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.acm.Axis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AxisApplicationTests {
 
 	@Test
 	void contextLoads() {
