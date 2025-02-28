1554cea685102c113575df4e06aea7c80248a85a
AnkitRaj2 <88496948+AnkitRaj2@users.noreply.github.com>
2025-02-28 16:44:39 -0600

    Basic navbar

--- Axis/frontend/axis/src/app/shared/components/navbar/navbar.component.ts
+++ Axis/frontend/axis/src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  dropdownToggle: boolean = true;
+  loggedIn: boolean = false;
 }
