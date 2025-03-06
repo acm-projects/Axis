df0f23c63cc0ab74a59709b28a767e697f51acac
AnkitRaj2 <88496948+AnkitRaj2@users.noreply.github.com>
2025-03-05 18:51:10 -0600

    Configured HTTP service

--- Axis/frontend/axis/src/app/features/sign-in/sign-in.component.ts
+++ Axis/frontend/axis/src/app/features/sign-in/sign-in.component.ts
@@ -1,11 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  signIn() : void {
+    // console.log('email: ' + this.email + ', password: ' + this.password);
+
+    /*
+      TODO: Validate email and password
+            Make HTTP request to backend to sign-in
+    */
+  }
 }
