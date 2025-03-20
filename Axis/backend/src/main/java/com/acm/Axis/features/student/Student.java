03c0e6babd610104e64ee668fa6e5d31b2cbb1e1
AnkitRaj2 <88496948+AnkitRaj2@users.noreply.github.com>
2025-03-20 00:01:37 -0500

    Sign-in and sign-up pages

--- Axis/backend/src/main/java/com/acm/Axis/features/student/Student.java
+++ Axis/backend/src/main/java/com/acm/Axis/features/student/Student.java
@@ -15,10 +15,10 @@ public record Student(
         if (email == null) throw new IllegalArgumentException("email cannot be null");
         if (first_name == null) throw new IllegalArgumentException("first_name cannot be null");
         if (last_name == null) throw new IllegalArgumentException("last_name cannot be null");
-        if (phone_number == null) throw new IllegalArgumentException("phone_number cannot be null");
-        if (gpa == null) throw new IllegalArgumentException("GPA cannot be null");
-        if (sat_score == null) throw new IllegalArgumentException("SAT_score cannot be null");
-        if (act_score == null) throw new IllegalArgumentException("ACT_score cannot be null");
+//        if (phone_number == null) throw new IllegalArgumentException("phone_number cannot be null");
+//        if (gpa == null) throw new IllegalArgumentException("GPA cannot be null");
+//        if (sat_score == null) throw new IllegalArgumentException("SAT_score cannot be null");
+//        if (act_score == null) throw new IllegalArgumentException("ACT_score cannot be null");
         if (password == null) throw new IllegalArgumentException("Password cannot be null");
     }
 
--- Axis/frontend/axis/src/app/features/sign-up/sign-up.component.ts
+++ Axis/frontend/axis/src/app/features/sign-up/sign-up.component.ts
@@ -1,11 +1,67 @@
 import { Component } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { NgxMaskDirective } from 'ngx-mask'
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [FormsModule, NgxMaskDirective, CommonModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
 
+  invalidDetails: boolean;
+
+  constructor(private http: HttpClient, private authService: AuthService, private router: Router) {
+    this.firstName = '';
+    this.lastName = '';
+    this.phoneNumber = '';
+    this.email = '';
+    this.password = '';
+    this.confirmPassword = '';
+    this.invalidDetails = false;
+  }
+
+  signUp() : void {
+    this.http.post('http://localhost:8080/auth/register',
+      {
+        first_name: this.firstName,
+        last_name: this.lastName,
+        phone_number: this.phoneNumber,
+        email: this.email,
+        password: this.password
+      }
+    ).subscribe({
+      next: (signUpResponse: any) => {
+
+        this.http.post('http://localhost:8080/auth/login',
+          {
+            email: this.email,
+            password: this.password
+          }
+        ).subscribe((loginResponse: any) => {
+            this.authService.saveSession({
+              email: this.email,
+              token: loginResponse.token
+            });
+            this.router.navigate(['/']); // Redirect to home page
+          });
+
+
+      },
+      error: error => {
+        this.invalidDetails = true;
+      }
+    });
+  }
 }
